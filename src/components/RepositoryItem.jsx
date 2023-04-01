9668d92c5ad91d658ef9b864682eae019de6507b
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-04-01 18:38:33 -0500

    Componente ítem para presentar los datos de un ítem de repositorio

---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const RepositoryItem = (item) => (
+    <View key={item.id} style={{ padding: 20, paddingBottom: 5, paddingTop: 5 }}>
+        <Text style={{ fontWeight: 'bold', marginBottom: 5 }}>ID: {item.id}</Text>
+        <Text>FullName: {item.fullName}</Text>
+        <Text>Descripcpion: {item.description}</Text>
+        <Text>Language: {item.language}</Text>
+        <Text>Stars: {item.stargazersCount}</Text>
+        <Text>Forks: {item.forksCount}</Text>
+        <Text>Review: {item.reviewCount}</Text>
+        <Text>Rating: {item.ratingAverage}</Text>
+    </View>
+);
+
+export default RepositoryItem;
--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { FlatList, View, Text } from 'react-native';
+import { FlatList, Text } from 'react-native';
 import repositories from '../data/repositories';
+import RepositoryItem from './RepositoryItem';
 
 const RepositoryList = () => {
     return (
@@ -8,19 +9,9 @@ const RepositoryList = () => {
             data={repositories}
             ItemSeparatorComponent={() => <Text> </Text>}
             renderItem={({ item: e }) => (
-                <View key={e.id} style={{padding: 20, paddingBottom: 5, paddingTop: 5}}>
-                    <Text style={{fontWeight: 'bold', marginBottom: 5}}>ID: {e.id}</Text>
-                    <Text>FullName: {e.fullName}</Text>
-                    <Text>Descripcpion: {e.description}</Text>
-                    <Text>Language: {e.language}</Text>
-                    <Text>Stars: {e.stargazersCount}</Text>
-                    <Text>Forks: {e.forksCount}</Text>
-                    <Text>Review: {e.reviewCount}</Text>
-                    <Text>Rating: {e.ratingAverage}</Text>
-                </View>
+                <RepositoryItem {...e} />
             )}
-        >
-        </FlatList>
+        />
     );
 }
 
