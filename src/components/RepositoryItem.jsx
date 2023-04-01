f426ccda20c4787f274d0f0bc2ec69f4ac24a7d8
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-04-01 18:42:18 -0500

    Estilos independientes para los componentes

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 const RepositoryItem = (item) => (
-    <View key={item.id} style={{ padding: 20, paddingBottom: 5, paddingTop: 5 }}>
-        <Text style={{ fontWeight: 'bold', marginBottom: 5 }}>ID: {item.id}</Text>
+    <View key={item.id} style={styles.container}>
+        <Text style={styles.strong}>ID: {item.id}</Text>
         <Text>FullName: {item.fullName}</Text>
         <Text>Descripcpion: {item.description}</Text>
         <Text>Language: {item.language}</Text>
@@ -13,5 +13,18 @@ const RepositoryItem = (item) => (
         <Text>Rating: {item.ratingAverage}</Text>
     </View>
 );
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 20,
+        paddingBottom: 5,
+        paddingTop: 5
+    },
+    strong: {
+        color: '#09f',
+        fontWeight: 'bold',
+        marginBottom: 5
+    }
+});
 
 export default RepositoryItem;
