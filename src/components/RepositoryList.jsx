b6dd7344c6039a19c2d340d1f45ef4895f5ef4ac
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-04-01 16:14:42 -0500

    Renderizado de elementos con la propiedad renderItem

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -6,15 +6,17 @@ const RepositoryList = () => {
     return (
         <FlatList
             data={repositories}
+            ItemSeparatorComponent={() => <Text> </Text>}
             renderItem={({ item: e }) => (
-                <View key={e.id}>
-                    <Text>{e.fullName}</Text>
-                    <Text>{e.description}</Text>
-                    <Text>{e.language}</Text>
-                    <Text>{e.stargazersCount}</Text>
-                    <Text>{e.forksCount}</Text>
-                    <Text>{e.reviewCount}</Text>
-                    <Text>{e.ratingAverage}</Text>
+                <View key={e.id} style={{padding: 20, paddingBottom: 5, paddingTop: 5}}>
+                    <Text style={{fontWeight: 'bold', marginBottom: 5}}>ID: {e.id}</Text>
+                    <Text>FullName: {e.fullName}</Text>
+                    <Text>Descripcpion: {e.description}</Text>
+                    <Text>Language: {e.language}</Text>
+                    <Text>Stars: {e.stargazersCount}</Text>
+                    <Text>Forks: {e.forksCount}</Text>
+                    <Text>Review: {e.reviewCount}</Text>
+                    <Text>Rating: {e.ratingAverage}</Text>
                 </View>
             )}
         >
