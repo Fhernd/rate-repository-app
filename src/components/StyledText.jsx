ef43e3f9dbdefc48242a8a3f0b9a499de322a598
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-04-01 18:49:42 -0500

    Componente personalizado de texto con estilo

--- src/components/StyledText.jsx
+++ src/components/StyledText.jsx
@@ -19,3 +19,20 @@ const styles = StyleSheet.create({
         fontSize: 10,
     },
 });
+
+export default function StyledText({ blue, bold, children, big, small }) {
+    const textStyles = [
+        styles.text,
+        blue && styles.blue,
+        bold && styles.bold,
+        big && styles.big,
+        small && styles.small,
+    ];
+
+    return (
+        <Text
+            style={textStyles}>
+            {children}
+        </Text>
+    );
+}
