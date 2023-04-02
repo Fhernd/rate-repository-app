464df21f33c66ff4305371a2c115be151ec44fd1
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-04-02 08:18:30 -0500

    Parsear millares para simplificar números en una vista

--- src/components/RepositoryStats.jsx
+++ src/components/RepositoryStats.jsx
@@ -3,15 +3,19 @@ import { View } from "react-native";
 
 import StyledText from "./StyledText";
 
+const parseThousands = (value) => {
+    return value > 999 ? `${(value / 1000).toFixed(1)}k` : value;
+};
+
 const RepositoryStats = item => (
     <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
         <View>
             <StyledText align='center' fontWeight='bold'>Stars</StyledText>
-            <StyledText align='center'>{item.stargazersCount}</StyledText>
+            <StyledText align='center'>{parseThousands(item.stargazersCount)}</StyledText>
         </View>
         <View>
             <StyledText align='center' fontWeight='bold'>Forks</StyledText>
-            <StyledText align='center'>{item.forksCount}</StyledText>
+            <StyledText align='center'>{parseThousands(item.forksCount)}</StyledText>
         </View>
         <View>
             <StyledText align='center' fontWeight='bold'>Review</StyledText>
