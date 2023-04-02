8be571d1e22234e07578a4bf4bedbc9a2457b4e8
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-04-02 12:19:17 -0500

    Configuración de WebPack

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TouchableWithoutFeedback } from 'react-native';
-
+import { NativeRouter } from 'react-router-native';
 import Main from './src/components/Main';
 
 export default function App() {
-  const nombre = 'L&J';
-
-  return <Main />;
+  return <NativeRouter><Main /></NativeRouter>;
 }
 
 const styles = StyleSheet.create({
