cf6ed376434fc3af9a643a44146302f4934785e3
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-04-02 12:23:42 -0500

    Inclusión de rutas de navegación con Native Router

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Constants from 'expo-constants';
 import { Text, View } from 'react-native';
+import { Switch } from 'react-native-web';
+import { Route } from 'react-router-native';
+
 import RepositoryList from './RepositoryList';
 import AppBar from './AppBar';
 
@@ -8,7 +11,15 @@ const Main = () => {
     return (
         <View style={{flex: 1}}>
             <AppBar />
-            <RepositoryList />
+            <Switch>
+                <Route path="/" exact>
+                    <RepositoryList />
+                </Route>
+                <Route path="/signin" exact>
+                    <Text>Sign in</Text>
+                </Route>
+                <Redirect to="/" />
+            </Switch>
         </View>
     );
 }
